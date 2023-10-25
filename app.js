9b4abe5c09010ff550c5fa0004b16bfbb1a3a1c5
Li Zhu <120238299+pigsswim@users.noreply.github.com>
2023-10-25 18:51:27 +0800

    Create app.js
    3rd commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    let x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
