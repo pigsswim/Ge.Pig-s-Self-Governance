a773f2621d6b7e6ce315534a78001a5a6d7f761c
Li Zhu <120238299+pigsswim@users.noreply.github.com>
2023-10-26 22:04:02 +0800

    Update app.js
    4th commit

--- app.js
+++ app.js
@@ -1,103 +1,52 @@
-function myFunction() {
-    let x = document.getElementById("myDIV");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
+
+ let fitness = [{
+  date: 'ramen',
+  food: 'ramen',
+  calorie:'500',
+  exercise: '500',
+  study: '500',
+  talking: '500',
+  phone:'500',
+  mood: '-1',
+  health: '50kg'
+ }]
+ 
+ function myFunction() {
+  var x = document.getElementById("health-management");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
+};
 
-  
-let healthManagement = [{
-  date: '2021-10-04',
-  food: 'Ramen',
-  calorie: '400',
-  talking: '60',
-  exercise: '60',
-  learning: '8',
-  mobileTrash: '60',
-  id: 'id1'
-}, {
-  date: '2021-10-04',
-  food: 'Ramen',
-  calorie: '400',
-  talking: '60',
-  exercise: '60',
-  learning: '8',
-  mobileTrash: '60',
-  id: 'id2'
-}];
-
-function createRow (date, food, calorie, talking, exercise, learning, mobileTrash,){
-  const id = ' ' + new Date().getTime(); 
-
-  healthManagement.push({
-    date: date,
-    food: food,
-    calorie: calorie,
-    talking: talking,
-    exercise: exercise,
-    learning: learning,
-    mobileTrash: mobileTrash,
-    id: id
-  });
-}
-
-function removeRow(idToDelete){
-  healthManagement = healthManagement.filter (function (weekDay){
-    if (weekDay.id === idToDelete){
-      return false;
-    } else {
-      return true;
-    }
-  });
-}
-
-function addRow (){
-  const dateCell = document.getElementById('date');
+ function addRow(){
+  const dateCell = document.getElementById('date-picker');
   const date = dateCell.value;
   const foodCell = document.getElementById('food');
   const food = foodCell.value;
   const calorieCell = document.getElementById('calorie');
   const calorie = calorieCell.value;
-  const talkingCell = document.getElementById('talking');
-  const talking = talkingCell.value;
   const exerciseCell = document.getElementById('exercise');
   const exercise = exerciseCell.value;
-  const learningCell = document.getElementById('learning');
-  const learning = learningCell.value;
-  const mobileTrashCell = document.getElementById('mobile-trash');
-  const mobileTrash = mobileTrashCell.value;
+  const studyCell = document.getElementById('study');
+  const study = studyCell.value;
+  const talkingCell = document.getElementById('talking');
+  const talking = talkingCell.value;
+  const phoneCell = document.getElementById('phone');
+  const phone = phoneCell.value;
+  const moodCell = document.getElementById('mood');
+  const mood = moodCell.value;
+  const healthCell = document.getElementById('health');
+  const health = healthCell.value;
+  const element = document.createElement('div');
+  element.innerText = date + food + calorie + exercise + study + 
+  talking + phone + mood + health;
+  element.style.cssText = `
+  color: red;
+  `;
+  document.body.appendChild(element);
+ };
 
-  createRow(date, food, calorie, talking, exercise, learning, mobileTrash);
-  render();
+ addRow();
 
-}
-
-function deleteRow(event){
-  const deleteButton = event.target;
-  const idToDelete = deleteButton.id;
-  removeRow(idToDelete);
-  render();
-  
-}
-
-function render(){
-  document.getElementById('health-management-week-day').innerHTML ='';
-  healthManagement.forEach(function (weekDay){
-    const element = document.createElement('div');
-    element.innerText = weekDay.date + ' ' + weekDay.food 
-    + weekDay.calorie + weekDay.talking + weekDay.exercise + weekDay.learning
-    + weekDay.mobileTrash;
-
-    const deleteButton = document.createElement('button');
-    deleteButton.innerText = 'Delete';
-    deleteButton.onclick = deleteRow;
-    deleteButton.id = weekDay.id;
-    element.appendChild(deleteButton);
-
-    const healthManagementTable = document.getElementById('health-management-table');
-    healthManagementTable.appendChild(weekDay);
-  
-  })
-}
