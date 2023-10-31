ece2f5ea3347dc777b2335d874cb11891dba3cd2
Li Zhu <120238299+pigsswim@users.noreply.github.com>
2023-10-31 13:36:51 +0800

    Update teresa.js
    3rd commit

--- teresa.js
+++ teresa.js
@@ -1,39 +1,109 @@
 
-let dateRow;
-let contentRow;
-let statusRow;
-
-const savedDateRow = JSON.parse(localStorage.getItem('dateRow'));
-const savedContentRow = JSON.parse(localStorage.getItem('contentRow'));
-const savedStatusRow = JSON.parse(localStorage.getItem('statusRow'));
-
-function addRow(){
-    const dateClm = document.getElementById('date-column');
+/*function addRow(){
     const datePicker = document.getElementById('date-picker');
+    const dateClm = document.getElementById('date-column');
     const dateRow = document.createElement('div');
     dateRow.setAttribute('class','btm-clm-row');
     dateRow.innerText = datePicker.value;
 
-    const contentClm = document.getElementById('content-column');
     const contentPicker = document.getElementById('content-picker');
+    const contentClm = document.getElementById('content-column');
     const contentRow = document.createElement('div');
-    contentRow.innerText = contentPicker.value;
     contentRow.setAttribute('class','btm-clm-row');
+    contentRow.innerText = contentPicker.value;
 
-    const statusClm = document.getElementById('status-column');
-    const statusPicker = document.getElementById('status-picker');
-    const statusRow = document.createElement('div');
-    statusRow.setAttribute('class','btm-clm-row');
-    statusRow.innerText = statusPicker.value;
-
+    const progressPicker = document.getElementById('progress-picker');
+    const progressClm = document.getElementById('progress-column');
+    const progressRow = document.createElement('div');
+    progressRow.setAttribute('class','btm-clm-row');
+    progressRow.innerText = progressPicker.value;;
 
     dateClm.appendChild(dateRow);
     contentClm.appendChild(contentRow);
-    statusClm.appendChild(statusRow);   
+    progressClm.appendChild(progressRow);    
+}*/
+
+let records = [];
+
+function createRecord (date,content,progress){
+    const id = '' + new Date().getTime();
+    records.push ({
+        date: date,
+        content: content,
+        progress: progress,
+        id: id
+    });
 }
 
-function saveRow (){
-    localStorage.setItem('dateRow',JSON.stringify(dateRow));
-    localStorage.setItem('contentRow',JSON.stringify(contentRow));
-    localStorage.setItem('statusRow',JSON.stringify(statusRow));
+function removeRecord (idToDelete){
+    records = records.filter (function (record){
+        if (record.id === idToDelete) {
+            return false;
+        } else {
+            return true;
+        };
+    });
 }
+
+function addRecord (){
+    const datePicker = document.getElementById('date-picker');
+    const date = datePicker.value;
+
+    const contentPicker = document.getElementById('content-picker');
+    const content = contentPicker.value;
+
+    const progressPicker = document.getElementById('progress-picker');
+    const progress = progressPicker.value;
+
+    createRecord (date, content, progress);
+    render();
+}
+
+function deleteRecord(event) {
+    const deleteButton = event.target;
+    const idToDelete = deleteButton.id;
+    
+    removeRecord(idToDelete);
+    render();
+}
+
+function render (){
+
+    document.getElementById('date-btm-clm-row-btm').innerHTML = '';
+    document.getElementById('content-btm-clm-row-btm').innerHTML = '';
+    document.getElementById('progress-btm-clm-row-btm').innerHTML = '';
+    document.getElementById('delete-btm-clm-row-btm').innerHTML = '';
+
+    records.forEach ((record) =>{
+        
+        const dateRow = document.createElement('div');
+        dateRow.setAttribute('class','btm-clm-row');
+        dateRow.innerText = record.date;
+        
+        const contentRow = document.createElement('div');
+        contentRow.setAttribute('class','btm-clm-row');
+        contentRow.innerText = record.content;
+        
+        const progressRow = document.createElement('div');
+        progressRow.setAttribute('class','btm-clm-row');
+        progressRow.innerText = record.progress;
+        
+        const deleteRow = document.createElement('div');
+        const deleteButton = document.createElement('button');
+        deleteButton.id = record.id;
+        deleteButton.onclick = deleteRecord;
+        deleteRow.setAttribute('class','btm-clm-row');
+        deleteButton.innerText = 'Delete';
+        deleteRow.appendChild(deleteButton);
+
+        const dateClm = document.getElementById('date-btm-clm-row-btm');
+        dateClm.appendChild(dateRow);
+        const contentClm = document.getElementById('content-btm-clm-row-btm');
+        contentClm.appendChild(contentRow);
+        const progressClm = document.getElementById('progress-btm-clm-row-btm');
+        progressClm.appendChild(progressRow);
+        const deleteClm = document.getElementById('delete-btm-clm-row-btm');
+        deleteClm.appendChild(deleteRow);
+    })
+}
+
