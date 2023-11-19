74d8623d7ca347258d08dbad727371186a3a6e34
LI ZHU <120238299+pigsswim@users.noreply.github.com>
2023-11-19 13:12:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,134 +1,60 @@
 
 let colors= [];
 const clickMe = document.getElementById('click-me');
-clickMe.addEventListener('click',createButton);
+clickMe.addEventListener('click',render);
 
-function generateColor() {
-    for (let i = 0; i < 10; i++) {
-        const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-        colors.push(randomColor);
-}
-    if (colors.length >10) {
-    colors = colors.slice(9,-1);
-}
-}
-
-function createButton() {
-    generateColor()
-    render()
-}
-
-setInterval(createButton,2000);
-
-function goHome() {
-    window.location.href = 'home.html';
+function mouseOver() {
+     document.getElementById('home-button').style.background = 'pink';
 }
 
 function render() {
+
     document.getElementById('ball-container2').innerHTML = '';
     document.getElementById('ball-container4').innerHTML = '';
+    document.getElementById('ball-container5').innerHTML = '';
     document.getElementById('ball-container6').innerHTML = '';
     document.getElementById('ball-container8').innerHTML = '';
 
-    const homeButton = document.createElement('button');
-    const homeButtonSpan = document.createElement('span');
-    homeButtonSpan.innerText = 'Home';
-    homeButtonSpan.style.cssText = `
-    color: #f2edd8;
-      background-image: linear-gradient(0deg, #d73936 0%, #f2edd8 100%);
-      -webkit-background-clip: text;
-      background-clip: text;
-      filter: drop-shadow(0 2px 2px rgb(80, 9, 12));
-      `
-
-    homeButton.appendChild(homeButtonSpan);
-    homeButton.style.cssText = `
-    background-color: ${colors[2]};
-    height: 100px;
-    width: 100px;
-    font-size: 20px;
-    color: #f2edd6;
-    cursor: pointer;
-    position: relative;
-    border-radius: 60px;
-    font-weight: 600;
-    border: 1px solid ${colors[2]};
-    background-image: linear-gradient(-180deg, ${colors[2]} 0%, ${colors[2]} 100%);
-    box-shadow: 0 16px 20px 0 rgba(113, 26, 19, 0.5),
-                              0 -4px 20px rgb(84, 4, 4) inset,
-                              0 12px 8px rgba(98, 23, 23, 0.4) inset,
-                              0 4px 8px 0 rgb(141, 39, 44) inset;
-    
-    `
+    const homeButton =document.createElement('button');
+    homeButton.id = 'home-button';
+    homeButton.setAttribute('class','home-button');
+    const homeLink = document.createElement('a');
+    homeButton.appendChild(homeLink);
+    homeLink.innerText = 'Home';
+    homeLink.href = 'home.html';
+    homeLink.id ='home-link';
+    homeLink.setAttribute('class','home-link');
+    homeLink.addEventListener('mouseover',mouseOver);
 
     const healthButton = document.createElement('button');
-    const healthButtonSpan = document.createElement('span');
-    healthButtonSpan.innerText = 'Health';
-    healthButton.appendChild(healthButtonSpan);
-    healthButton.style.cssText = `
-    background-color: ${colors[4]};
-    height: 100px;
-    width: 100px;
-    font-size: 20px;
-    color: #f2edd6;
-    cursor: pointer;
-    position: relative;
-    border-radius: 60px;
-    font-weight: 600;
-    border: 1px solid ${colors[4]};
-    background-image: linear-gradient(-180deg, ${colors[4]} 0%, ${colors[4]} 100%);
-    box-shadow: 0 16px 20px 0 rgba(113, 26, 19, 0.5),
-                              0 -4px 20px rgb(84, 4, 4) inset,
-                              0 12px 8px rgba(98, 23, 23, 0.4) inset,
-                              0 4px 8px 0 rgb(141, 39, 44) inset;
-    
-    `
+    healthButton.id = 'health-button';
+    healthButton.setAttribute('class','health-button');
+    const healthLink = document.createElement('a');
+    healthButton.appendChild(healthLink);
+    healthLink.innerText = 'Health';
+    healthLink.href = 'health.html';
+    healthLink.id ='health-link';
+    healthLink.setAttribute('class','health-link');
 
     const financeButton = document.createElement('button');
-    const financeButtonSpan = document.createElement('span');
-    financeButtonSpan.innerText = 'Finance';
-    financeButton.appendChild(financeButtonSpan);
-    financeButton.style.cssText = `
-    background-color: ${colors[6]};
-    height: 100px;
-    width: 100px;
-    font-size: 20px;
-    color: #f2edd6;
-    cursor: pointer;
-    position: relative;
-    border-radius: 60px;
-    font-weight: 600;
-    border: 1px solid ${colors[6]};
-    background-image: linear-gradient(-180deg, ${colors[6]} 0%, ${colors[6]} 100%);
-    box-shadow: 0 16px 20px 0 rgba(113, 26, 19, 0.5),
-                              0 -4px 20px rgb(84, 4, 4) inset,
-                              0 12px 8px rgba(98, 23, 23, 0.4) inset,
-                              0 4px 8px 0 rgb(141, 39, 44) inset;
-    
-    `
+    financeButton.id = 'finance-button';
+    financeButton.setAttribute('class','finance-button');
+    const financeLink = document.createElement('a');
+    financeButton.appendChild(financeLink);
+    financeLink.innerText = 'finance';
+    financeLink.href = 'finance.html';
+    financeLink.id ='finance-link';
+    financeLink.setAttribute('class','finance-link');
 
     const cognitivityButton = document.createElement('button');
-    const cognitivityButtonSpan = document.createElement('span');
-    cognitivityButtonSpan.innerText = 'Cognitive';
-    cognitivityButton.appendChild(cognitivityButtonSpan);
-    cognitivityButton.style.cssText = `
-    background-color: ${colors[8]};
-    height: 100px;
-    width: 100px;
-    font-size: 20px;
-    color: #f2edd6;
-    cursor: pointer;
-    position: relative;
-    border-radius: 60px;
-    font-weight: 600;
-    border: 1px solid ${colors[8]};
-    background-image: linear-gradient(-180deg, ${colors[8]} 0%, ${colors[8]} 100%);
-    box-shadow: 0 16px 20px 0 rgba(113, 26, 19, 0.5),
-                              0 -4px 20px rgb(84, 4, 4) inset,
-                              0 12px 8px rgba(98, 23, 23, 0.4) inset,
-                              0 4px 8px 0 rgb(141, 39, 44) inset;
-    
-    `
+    cognitivityButton.id = 'cognitivity-button';
+    cognitivityButton.setAttribute('class','cognitivity-button');
+    const cognitivityLink = document.createElement('a');
+    cognitivityButton.appendChild(cognitivityLink);
+    cognitivityLink.innerText = 'cognitivity';
+    cognitivityLink.href = 'cognitivity.html';
+    cognitivityLink.id ='cognitivity-link';
+    cognitivityLink.setAttribute('class','cognitivity-link');
     
     const ballContainer2 = document.getElementById('ball-container2');
     ballContainer2.appendChild(homeButton);
@@ -142,8 +68,3 @@ function render() {
     const ballContainer8 = document.getElementById('ball-container8');
     ballContainer8.appendChild(cognitivityButton);
 }
-
-//homeButton.onclick = window.location.href = 'home.html';
-/*
-
-                              */
