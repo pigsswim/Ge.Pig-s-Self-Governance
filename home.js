d02c6887ebf2b03b7ef74a319d392e0b95cf6156
LI ZHU <120238299+pigsswim@users.noreply.github.com>
2023-12-03 20:10:06 +0800

    Update home.js

--- home.js
+++ home.js
@@ -63,12 +63,64 @@ function scrollToHealth () {
 
 //color animation 
 
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
 
+var ok2animate = true;
 
+function Drop() {
+    this.x = Math.random() * (canvas.width - 20);
+    this.y = -Math.random() * 180;
+    this.fallRate = Math.random()*.5+.5;
+}
+//
+Drop.prototype.draw = function () {
+    ctx.beginPath();
+    ctx.moveTo(this.x - 5, this.y);
+    ctx.lineTo(this.x, this.y - 7);
+    ctx.lineTo(this.x + 5, this.y);
+    ctx.arc(this.x, this.y, 5, 0, Math.PI);
+    ctx.closePath();
+    ctx.fill();
+    return (this);
+}
+//
+Drop.prototype.fall = function () {
+    this.y += this.fallRate;
+    return (this);
+}
 
+function animate() {
 
+    // request another animation frame
+    if (ok2animate) {
+        requestAnimationFrame(animate);
+    }
 
+    // fill the canvas with the orange background
+        ctx.fillStyle="black";
+        ctx.fillRect(0,0,canvas.width,canvas.height)
+    //ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    // make all drops fall and then redraw them
+    ctx.fillStyle = "white";
+    for (var i = 0; i < drops.length; i++) {
+        drops[i].fall().draw();
+    }
+
+}
+
+// an array of objects each representing 1 drop
+var drops = [];
+
+// add some test drops
+for (var i = 0; i < 50; i++) {
+    drops.push(new Drop());
+}
+
+requestAnimationFrame(animate);
+
+//sunshine
 
 // homepage one section in the middle 
 // render audio 
