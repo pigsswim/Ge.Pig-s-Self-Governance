0e8eff65a842cf39db4d47decd0074bcfdcacb51
LI ZHU <120238299+pigsswim@users.noreply.github.com>
2024-01-08 20:16:12 +0800

    Update app.js

--- English/intro/app.js
+++ English/intro/app.js
@@ -33,6 +33,6 @@ function renderBackground() {
 setTimeout(renderBackground,speed*10);
 
 function goHome() {
-  location.href = '../about/./';
+  location.href = '../about/index.html';
 }
 setTimeout(goHome,speed*11);
