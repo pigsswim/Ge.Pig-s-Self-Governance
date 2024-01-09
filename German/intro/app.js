aea7a11d2d4161d0421da8e1cdd09955a10a8e3f
Li Zhu <120238299+pigsswim@users.noreply.github.com>
2024-01-09 19:24:19 +0800

    909042024

---
+++ German/intro/app.js
@@ -0,0 +1,38 @@
+// onload call javascript function 
+let timer =0;
+let speed = 1000;
+let i = 0;
+let curtain = document.getElementById('curtain');
+let screen = document.getElementById('screen');
+let title = document.createElement('h1');
+
+function intro () {
+  const curtain = document.createElement('div');
+  curtain.id = 'curtain';
+  curtain.className ='curtain';
+  document.body.appendChild(curtain)
+  screen.onload = setTimeout(typeTitle,1000);
+  curtain.appendChild(title);
+}
+
+intro();
+
+function typeTitle () {
+  let a = 'Die gechichte die wer ich habe keine idee';
+  if (i<a.length){
+    title.innerHTML += a.charAt(i);
+    i++;
+  }
+  setTimeout(typeTitle, 60);
+}
+
+function renderBackground() {
+  document.body.innerHTML = '';
+}
+
+setTimeout(renderBackground,speed*10);
+
+function goHome() {
+  location.href = '../about/./';
+}
+setTimeout(goHome,speed*11);
