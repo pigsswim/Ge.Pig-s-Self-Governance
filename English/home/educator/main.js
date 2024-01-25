2af412a829dfbaad1001623d5e371cf306d1f84b
Li Zhu <120238299+pigsswim@users.noreply.github.com>
2024-01-25 16:41:45 +0800

    3-25012024

--- English/home/educator/main.js
+++ English/home/educator/main.js
@@ -1,21 +1,31 @@
+
 let set = document.getElementById('set');
-set.addEventListener('click',generateStory());
+set.addEventListener('click',generateStory())
 
-//model 
-localStorage.clear();
-
-function generateStory () {
+function generateStory() {
         let role = document.getElementById('name');
-        let story = document.getElementById('story');
-        let storyText = story.innerText;
-        let newStory = storyText.replaceAll('Julie',`${role.value}`);
-        let title1 = document.getElementById('title1');
-        let title1Text = title1.innerText;
-        let newTitle1 = title1Text.replaceAll('Julie',`${role.value}`);
-        title1.innerText = newTitle1;
-        story.innerHTML = newStory;
+        let newRole = role.value;
+        let text = document.getElementsByTagName('main')
+        let moduleWrapper = document.createElement('div')
+        moduleWrapper.setAttribute('class', 'module-wrapper')
+        let unitWrapper = document.createElement('div')
+        unitWrapper.setAttribute('class','unit-wrapper')
+        let unit1 = document.createElement('div')
+        unit1.setAttribute('class','unit')
+        unit1.setAttribute('id','title')
+        let p1 = document.createElement('p')
+        p1.innerText = `How many hours does ${newRole} poop? `;
+        let unit2 = document.createElement('div')
+        unit2.setAttribute('class','unit')
+        unit2.setAttribute('id','content')
+        let p2 = document.createElement('p')
+        unit1.appendChild(p1)
+        unit2.appendChild(p2)
+        unitWrapper.appendChild(unit1)
+        unitWrapper.appendChild(unit2)
+        moduleWrapper.appendChild(unitWrapper)
+        text.innerHTML = moduleWrapper;
 }
-        
 //view
 
 
